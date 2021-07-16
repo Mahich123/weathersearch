605502df0272a1913b44f336a996499fe5ebaa1a
Mahi <mahichowdhur7@gmail.com>
2021-07-16 22:26:42 +0600

    added weather_search_app

---
+++ app.js
@@ -0,0 +1,20 @@
+var Submit = document.querySelector('.submit');
+var inputValue = document.querySelector('.inputValue');
+var Name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+Submit.addEventListener('click', function() {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=55df72f901718e32c0b3571b59b00475'+'&units=metric')
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+       
+        var descValue = data['weather'][0]['description'];
+       
+        Name.innerHTML = nameValue;
+        temp.innerHTML = tempValue + `°C`;
+        desc.innerHTML = descValue;
+    })
+})
